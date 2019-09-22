5e7e8306ea07f018d1b9cbe2557f58e0ff6e2ebd
thutruongvn <thutruong.1986@gmail.com>
2019-09-21 22:55:25 -0500

    commit logout function

--- client/app/logout/logout.component.ts
+++ client/app/logout/logout.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  template: ''
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.logout();
   }
 
 }
