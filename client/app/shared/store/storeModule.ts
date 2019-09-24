06e47c36ddf71e25c55e3d87aa62f5a16ea22f63
thutruongvn <thutruong.1986@gmail.com>
2019-09-24 01:08:06 -0500

    update store

--- client/app/shared/store/storeModule.ts
+++ client/app/shared/store/storeModule.ts
@@ -1,7 +1,38 @@
 import { IAppState, INITIAL_STATE } from './appState';
-import { NgRedux } from '@angular-redux/store';
+import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
 import { rootReducer } from './reducer';
+import { NgModule } from '@angular/core';
 
+// @NgModule({
+//     imports: [NgReduxModule, NgReduxRouterModule],
+//     providers: [RootEpics],
+//   })
+//   export class StoreModule {
+//     constructor(
+//       public store: NgRedux<IAppState>,
+//       devTools: DevToolsExtension,
+//       ngReduxRouter: NgReduxRouter,
+//       rootEpics: RootEpics,
+//     ) {
+//       // Tell Redux about our reducers and epics. If the Redux DevTools
+//       // chrome extension is available in the browser, tell Redux about
+//       // it too.
+//       store.configureStore(
+//         rootReducer,
+//         {},
+//         [ createLogger(), ...rootEpics.createEpics() ],
+//         devTools.isEnabled() ? [ devTools.enhancer() ] : []);
+  
+//       // Enable syncing of Angular router state with our Redux store.
+//       if (ngReduxRouter) {
+//         ngReduxRouter.initialize();
+//       }
+  
+//       // Enable syncing of Angular form state with our Redux store.
+//       provideReduxForms(store);
+//     }
+//   }
+  
 export class AppModule {
     constructor (ngRedux: NgRedux<IAppState>) {
         ngRedux.configureStore(rootReducer, INITIAL_STATE);
